c4fdaeb0b8aad70c2e4fb8b012f42f2e89f0b394
Andrew DePace <93351057+Hyperion1232@users.noreply.github.com>
2022-03-02 15:15:37 -0500

    Create template.php

---
+++ template.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+	<link href="styles.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<!--- START OF NAVBAR---> 
+<?php
+include_once("navbar.php"); 
+?>
+<!--- END OF NAVBAR ---> 
+<div>
+<p id="ReportAnEmergency">Report an Emergency</p>
+</div>
+
+
+<!---THIS IS THE TEXT UNDER REPORT AN EMERGENCY ---> 
+<div>
+<p id = "step">Notifications</p>
+</div>
+<!--- THE CONTENT DEPENDS ON THE PAGE. SOME PAGES DONT EVEN 
+HAVE THIS TEXT SO CHECK WITH THE PAGE> ---> 
+<div>
+
+<!--- This is the partial line under the notifications tab and above the content
+    of the page. When editing this template, make sure the page has this because 
+    some pages do not --->
+<hr align="left">
+
+
+
+
+
+<!--- PUT CONTENT HERE---> 
+
+
+
+
+</div>
+</body>
+</html>
