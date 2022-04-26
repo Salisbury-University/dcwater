a95cf36d7ec045b081bc65b90d2a42968c58514b
Andrew DePace <93351057+Hyperion1232@users.noreply.github.com>
2022-04-26 10:19:51 -0400

    Update Step2.php

--- Step2.php
+++ Step2.php
@@ -58,14 +58,58 @@ padding-right: 17px;
 padding-bottom: 10px;
 }
 
-.bigbutton:active{
+.bigbuttonblue{
+font-family: 'Fira Sans', sans-serif;
+cursor: pointer;
+position: relative;
+width: 238px;
+height: 70px;
 background-color: #8EBDE0;
+color: #00528C;
+border-style: solid;
+border-width: 2px;
+border-radius: 16px 16px 16px 16px;
+border-color: #0075C9;
+right: 76.68%;
+left: 10px;
+top: 10px;
+font-size: 18px;
+padding-left: 100px;
+padding-top: 10px;
+padding-right: 17px;
+padding-bottom: 10px;
+box-shadow: 0px 3px 5px #808080;
 }
 
 .halfbutton{
 cursor: pointer;
 position: relative;
 background-color: #8EBDE0;
+width: 80px;
+height: 90px;
+border-radius: 16px 0px 0px 16px;
+left: 10px;
+top: -80px;
+}
+
+.halfbuttonactive{
+cursor: pointer;
+position: relative;
+background-color: #8EBDE0;
+width: 80px;
+height: 90px;
+border-top: 2px solid #0075C9;
+border-left: 2px solid #0075C9;
+border-bottom: 2px solid #0075C9;
+border-radius: 16px 0px 0px 16px;
+left: 10px;
+top: -84px;
+}
+
+.halfbuttongray{
+cursor: pointer;
+position: relative;
+background-color: #EFEFEF;
 width: 80px;
 height: 90px;
 border-radius: 16px 0px 0px 16px;
@@ -87,6 +131,9 @@ color: #2D2D2D;
 
 </style>
 </head>
+
+
+
 <body>
 <!--- START OF NAVBAR---> 
 <nav>
@@ -119,32 +166,85 @@ HAVE THIS TEXT SO CHECK WITH THE PAGE> --->
 
 <!--- PUT CONTENT HERE---> 
 
-<div class = "bigbutton">
+<div id = "Out" class = "bigbutton" onclick="ChangeColor(); swapImg(); activateOut();">
 Outdoors
 <p class = "rando">Report a problem located in an outdoor area</p>
 </div>
-<div class = "halfbutton">
+<div id = "HalfOut" class = "halfbutton">
 <img class = "icon" src="Images/Outdoor.png">
 </div>
 
-<div class = "bigbutton" style="margin-top:-75px">
+
+
+
+<div id = "In" class = "bigbutton" style="margin-top:-75px" onclick="ChangeColor(); swapImg(); activateIn();">
 Indoors
 <p class = "rando">Report a problem located inside a building</p>
 </div>
-<div class = "halfbutton">
+<div id = "HalfIn" class = "halfbutton">
 <img class = "icon" src="Images/Indoor.png">
 </div>
 
 <div style="font-family:'Fira Sans', sans-serif;font-size:18px;color:#2D2D2D;text-align:center;width:320px;height:80px;margin-left:25px;">
 If the options above do not apply, please 
-<a href="#">describe your issue here.</a>
+<a href="unsure.php">describe your issue here.</a>
 </div>
 
 <div class="bottomNav" style="margin-top:250px">
-<button onclick="#" type="button"  class="prevButton"><span class="prevButtonText"><img class="prevArrow" src="Images/ArrowBlue.png">Back</span></button>
-<button onclick="#" type="button" class="nextButton"><span>Next<img class="nextArrow" src="Images/ArrowWhite.png"></span></button>
+<button onclick="window.location.href = 'Location.php';" type="button"  class="prevButton"><span class="prevButtonText"><img class="prevArrow" src="Images/ArrowBlue.png">Back</span></button>
+<button onclick="#" type="button" id="ForColor" class="nextButton" disabled><span id="ForColor">Next<img class="nextArrow" id="ImgColor" src="Images/ArrowBlack.png"></span></button>
 </div>
 
 
 </body>
+<script type="text/javascript">
+
+document.getElementById("ForColor").style.background = "#DDDDDD";
+function ChangeColor(){
+if (document.getElementById("Out").value == "" || document.getElementById("In").value == ""){
+document.getElementById("ForColor").style.background = "#DDDDDD";
+} else {
+document.getElementById("ForColor").style.background = "#0075C9";
+document.getElementById("ForColor").style.color = "#FFFFFF";
+document.getElementById("ForColor").disabled = false;
+}
+
+}
+
+function swapImg(){
+if (document.getElementById("ImgColor").src.endsWith("ArrowBlack.png") == true){
+document.getElementById("ImgColor").src = "Images/ArrowWhite.png";
+} else {
+document.getElementById("ImgColor").src = "Images/ArrowWhite.png";
+}
+
+}
+
+function activateOut(){
+document.getElementById("Out").className = "bigbuttonblue";
+document.getElementById("HalfOut").className = "halfbuttonactive";
+document.getElementById("HalfIn").className = "halfbuttongray";
+document.getElementById("ForColor").onclick = function(){
+location.href = "Step3Out.php";
+}
+if (document.getElementById("In").classList.contains ("bigbuttonblue")){
+document.getElementById("In").className = "bigbutton";
+}
+}
+
+function activateIn(){
+document.getElementById("In").className = "bigbuttonblue";
+document.getElementById("HalfIn").className = "halfbuttonactive";
+document.getElementById("HalfOut").className = "halfbuttongray";
+document.getElementById("ForColor").onclick = function(){
+location.href = "Step3In.php";
+}
+if (document.getElementById("Out").classList.contains ("bigbuttonblue")){
+document.getElementById("Out").className = "bigbutton";
+}
+}
+
+
+</script>
+
 </html>
