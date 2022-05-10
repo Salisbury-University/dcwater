0efcdf25de72ca51106f1154a559efd212be7111
Alex Hearne <47182454+alexhearne@users.noreply.github.com>
2022-05-10 13:17:38 -0400

    Updated the ajax query

--- Review1.php
+++ Review1.php
@@ -118,7 +118,7 @@ document.getElementById("ThirdBoxText").innerHTML = sessionStorage.getItem("phot
  Back
 </span>
 </button>
-<button onclick="window.location='ThankYou.php'" type="button" class="nextButton" style="border-color: none;">
+<button onclick="window.location='ThankYou.php' submitQuery()" type="button" class="nextButton" style="border-color: none;">
 <span class="nextButtonText">Next
 <img src="Images/ArrowWhite.png" class="WhiteArrow">
 </span>
@@ -136,6 +136,42 @@ if (inOrOut.toString()=="other") {
 }
 }
 </script>    
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script type="text/javascript">
+	function submitQuery(){
+//session storage variables from location page 
+		let locationOf = sessionStorage.getItem('ColorChange');
+//session storage variables from Step3 page
+		let problem = sessionStorage.getItem('problemType');
+//session storage varaibles from additional details 
+		let cameraName = sessionStorage.getItem('photoName'); 
+		let descriptionOfProblem = sessionStorage.getItem('descriptionOfProblem');
+		let details = sessionStorage.getItem('details');
+//session storage varaibles from contactInfo page
+		let moreInfo = sessionStorage.getItem('moreInfo'); 
+		let contactWquestions = sessionStorage.getItem('contactQuestion'); 
+		let fullName = sessionStorage.getItem('fullName');
+		let email = sessionStorage.getItem('email'); 
+		let phoneNum = sessionStorage.getItem('phone');
+		let ticketNum = sessionStorage.getItem('ticketNumber');  
+$.post("uploadToServer.php", 
+      {
+      	location: locationOf, 
+      	problemType: problem, 
+        photoName: cameraName, 
+        description: descriptionOfProblem, 
+        tags: details, 
+        updates: moreInfo, 
+        contact: contactWquestions,
+        name: fullName, 
+        emailAddress: email,
+        phone:phoneNum,
+        ticketNumber:ticketNum
+       });
+sessionStorage.clear(); 
+}
+</script>
 
 </body>
 </html>
